e287e9b43626505f71242c5f8c701e3dcc89a5ab
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-02-23 16:42:44 +0100

    fix bugs for id inputs

--- app.py
+++ app.py
@@ -1,54 +1,77 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 from pyBiodatafuse.id_mapper import bridgedb_xref
+from components.graph_creation.id_input import process_identifiers
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # flash('Welcome to BioDataFuse!')
+    return render_template('about.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+ALLOWED_EXTENSIONS = {'txt', 'csv'}  # Define allowed file extensions
+
+def allowed_file(filename):
+    """Check if the file extension is allowed."""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/graph_creation', methods=['GET', 'POST'])
 def gc_id_input():
     if request.method == 'POST':
-        ids = request.form.get('text_input')
-        if not ids:
-            return render_template('gc_id_input.html', warnings='Please enter at least one identifier')
-        session['ids_df'] = ids
+        text_input = request.form.get('text_input')
+        file_upload = request.files.get('file_upload')
+
+        ids_df, warnings = process_identifiers(file_upload, text_input)
+
+        if ids_df:
+            session['ids_df'] = ids_df
+
+        if warnings:
+            return jsonify({'error': warnings})
+
         return redirect(url_for('gc_id_type'))
+   
     return render_template('gc_id_input.html')
 
 @app.route('/graph_creation/gc_id_type', methods=['GET', 'POST'])
 def gc_id_type():
+    ids_df = session.get('ids_df')
+
     if request.method == 'POST':
         id_type = request.form.get('identifier_type')
         if not id_type:
-            return render_template('gc_id_type.html', warnings='Please select an identifier type')
-        session['id_type'] = id_type
+            return jsonify({'error': 'Please select an identifier type'})
+        if id_type:
+            session['id_type'] = id_type
+
         return redirect(url_for('gc_id_mapper'))
-    return render_template('gc_id_type.html')
+    
+    return render_template('gc_id_type.html', ids_df=ids_df)
+
 
 @app.route('/graph_creation/gc_id_mapper', methods=['GET', 'POST'])
 def gc_id_mapper():
-    if 'ids' in session and 'id_type' in session:
-        if request.method == 'POST':
-            bridgdb_df, bridgdb_metadata = bridgedb_xref(
-                identifiers=session['ids_df'], 
-                input_datasource=['id_type'],
-                input_species="Human")
-            session['bridgdb_df'] = bridgdb_df
-            session['bridgdb_metadata'] = bridgdb_metadata
-            # Set session variable to indicate mapping step is done
-            session['mapping_done'] = True
-            return render_template('gc_datasource.html')
-        return render_template('gc_id_mapper.html')
-
-    else:
+    ids_df = session.get('ids_df')
+    id_type = session.get('id_type')
+    if not (ids_df and id_type):
         return redirect(url_for('gc_id_input'))
+
+    if request.method == 'POST':
+        bridgdb_df, bridgdb_metadata = bridgedb_xref(
+            identifiers=ids_df, 
+            input_datasource=[id_type],  # You need to pass the actual id_type, not the string 'id_type'
+            input_species="Human"
+        )
+        session['bridgdb_df'] = bridgdb_df
+        session['bridgdb_metadata'] = bridgdb_metadata
+        return redirect(url_for('gc_datasource'))
+    return render_template('gc_id_mapper.html', ids_df=session['ids_df'], id_type=session['id_type'])
 
 @app.route('/render_template')
 def render_template_route():
@@ -62,5 +85,14 @@ def render_template_route():
         # If any of the required session variables are missing, redirect to the input page
         return redirect(url_for('gc_id_input'))
     
+@app.route('/graph_analysis')
+def graph_analysis():
+    return render_template('graph_analysis.html')
+
+@app.route('/clear_session', methods=['POST'])
+def clear_session():
+    session.clear()
+    return 'Session cleared'
+
 if __name__ == '__main__':
     app.run(debug=True)
--- components/graph_creation/id_input.py
+++ components/graph_creation/id_input.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from io import StringIO
+from pyBiodatafuse.data_loader import create_df_from_text
 
 def process_identifiers(uploaded_file, text_input) -> pd.DataFrame:
     """convert the input identifier list to a dataframe.
@@ -13,25 +13,18 @@ def process_identifiers(uploaded_file, text_input) -> pd.DataFrame:
     warnings = []  # Initialize empty list for warnings
 
     # Process identifiers from text input
-    if text_input.strip():
-        identifiers_from_text = text_input.strip().split('\n')
-        identifiers_df['Identifier'] = identifiers_from_text
+    if text_input:
+        # Create a DataFrame from the list of identifiers
+        identifiers_df = create_df_from_text(text_input)
 
     # Process identifiers from uploaded file
     if uploaded_file:
-        # Ensure file type is CSV or TXT
-        if uploaded_file.filename.endswith(('.csv', '.txt')):
-            # Read the uploaded file
-            file_contents = uploaded_file.read().decode("utf-8")
-            identifiers_from_file = pd.read_csv(StringIO(file_contents), header=None, squeeze=True)
-            
-            # Append identifiers from file to DataFrame
-            identifiers_df = pd.concat([identifiers_df, identifiers_from_file])
-        else:
-            warnings.append("Unsupported file format. Please upload a CSV or TXT file.")
-
+        # Read the uploaded file
+        file_contents = uploaded_file.getvalue().decode("utf-8")
+        identifiers_df = pd.concat([identifiers_df, create_df_from_text(file_contents)], ignore_index=True).drop_duplicates()
+        
     # Check if neither uploaded file nor text input provided
     if identifiers_df.empty and not warnings:
         warnings.append("Please provide your identifiers!")
 
-    return identifiers_df, warnings
+    return identifiers_df.to_dict('records'), warnings
