1eb7f43324f4646d194a9385b0661011c609dba1
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2024-02-26 12:25:47 +0100

    fix id mapper step

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
-from pyBiodatafuse.id_mapper import bridgedb_xref
+from components.graph_creation.id_mapper import bridgedb_xref
 from components.graph_creation.id_input import process_identifiers
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -14,13 +15,6 @@ def index():
 def about():
     return render_template('about.html')
 
-ALLOWED_EXTENSIONS = {'txt', 'csv'}  # Define allowed file extensions
-
-def allowed_file(filename):
-    """Check if the file extension is allowed."""
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/graph_creation', methods=['GET', 'POST'])
 def gc_id_input():
     if request.method == 'POST':
@@ -31,6 +25,9 @@ def gc_id_input():
 
         if ids_df:
             session['ids_df'] = ids_df
+            ids_df = pd.DataFrame(ids_df)
+            no_input_ids = len(ids_df['identifier'].unique())
+            session['no_input_ids'] = no_input_ids
 
         if warnings:
             return jsonify({'error': warnings})
@@ -51,40 +48,34 @@ def gc_id_type():
             session['id_type'] = id_type
 
         return redirect(url_for('gc_id_mapper'))
-    
-    return render_template('gc_id_type.html', ids_df=ids_df)
-
-
-@app.route('/graph_creation/gc_id_mapper', methods=['GET', 'POST'])
-def gc_id_mapper():
-    ids_df = session.get('ids_df')
-    id_type = session.get('id_type')
-    if not (ids_df and id_type):
-        return redirect(url_for('gc_id_input'))
-
-    if request.method == 'POST':
-        bridgdb_df, bridgdb_metadata = bridgedb_xref(
-            identifiers=ids_df, 
-            input_datasource=[id_type],  # You need to pass the actual id_type, not the string 'id_type'
-            input_species="Human"
+ 
+    return render_template(
+        'gc_id_type.html',
+        ids_df=ids_df,
+        no_input_ids=session.get('no_input_ids')
         )
-        session['bridgdb_df'] = bridgdb_df
-        session['bridgdb_metadata'] = bridgdb_metadata
-        return redirect(url_for('gc_datasource'))
-    return render_template('gc_id_mapper.html', ids_df=session['ids_df'], id_type=session['id_type'])
 
-@app.route('/render_template')
-def render_template_route():
-    if 'bridgdb_df' in session and 'ids_df' in session and 'id_type' in session:
-        bridgdb_df = session['bridgdb_df']
-        ids_df = session['ids_df']
-        id_type = session['id_type']
-        bridgdb_df_empty = bridgdb_df["target"].str.strip().eq("").all()
-        return render_template('gc_id_mapper.html', bridgdb_df=bridgdb_df, ids_df=ids_df, id_type=id_type, bridgdb_df_empty=bridgdb_df_empty)
-    else:
-        # If any of the required session variables are missing, redirect to the input page
-        return redirect(url_for('gc_id_input'))
+@app.route('/graph_creation/gc_id_mapper')
+def gc_id_mapper():
+    ids_df = pd.DataFrame(session.get('ids_df'))
+    id_type = session.get('id_type')
+
+    bridgdb_df, bridgdb_metadata = bridgedb_xref(
+        identifiers=ids_df,
+        input_species="Human",
+        input_datasource=id_type
+    )
+
+    session['bridgdb_df'] = bridgdb_df.to_dict('records')
+    session['bridgdb_metadata'] = bridgdb_metadata
     
+    return render_template(
+        'gc_id_mapper.html',
+        ids_df=session['ids_df'],
+        id_type=session['id_type'],
+        no_input_ids=session['no_input_ids'],
+        bridgdb_df_empty = bridgdb_df.empty)
+  
 @app.route('/graph_analysis')
 def graph_analysis():
     return render_template('graph_analysis.html')
