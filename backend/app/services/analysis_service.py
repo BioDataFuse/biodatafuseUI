9b88a52e6588bf4da59f04f05b5cf5fd4cb33e96
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2025-06-27 17:41:51 +0200

    update analysis files in backend

--- backend/app/services/analysis_service.py
+++ backend/app/services/analysis_service.py
@@ -1,23 +1,46 @@
-# backend/app/services/graph_analysis_service.py
+from pathlib import Path
+from pyBiodatafuse.analyzer.summarize import BioGraph
+from backend.app.services.graph_service import GraphService
+from backend.app import models
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from pyBiodatafuse.viz.patent_data import get_patent_data, _process_data_for_plot
-from pyBiodatafuse import id_mapper
+class AnalysisService:
+    def __init__(self, db: AsyncSession):
+        self.db = db
 
-class GraphAnalysisService:
-    def __init__(self):
-        pass
+    async def get_graph_summary(
+            self, annotation: models.Annotation, graph_dir: Path):
+        try: 
+            pygraph, error = GraphService.create_pygraph(annotation, graph_dir)
+            if error:
+                return None, f"Graph error: {error}"
 
-    async def run_patent_analysis(self, chem_list):
-        # 1. Map compound names to PubChem CIDs
-        pubchem_df, _ = id_mapper.pubchem_xref(
-            identifiers=chem_list,
-            indentifier_type="name"
-        )
+            graph_obj = BioGraph(graph=pygraph)
+            return graph_obj.graph_summary, None
+        except Exception as e:
+            return None, {"message": f"Error generating graph summary: {str(e)}"}
 
-        # 2. Fetch patent metadata
-        metadata_df = get_patent_data(pubchem_df)
 
-        # 3. Format for plotting
-        processed_df = _process_data_for_plot(metadata_df)
+    async def plot_node_counts(self, annotation: models.Annotation, graph_dir: Path):
+        try:
+            pygraph, error = GraphService.create_pygraph(annotation, graph_dir)
+            if error:
+                return None, f"Graph error: {error}"
 
-        return processed_df
+            graph_obj = BioGraph(graph=pygraph)
+            fig = graph_obj.count_nodes_by_data_source(plot=True)
+            return fig, None
+        except Exception as e:
+            return None, {"message": f"Error generating node counts: {str(e)}"}
+
+    async def plot_edge_counts(self, annotation: models.Annotation, graph_dir: Path):
+        try:
+            pygraph, error = GraphService.create_pygraph(annotation, graph_dir)
+            if error:
+                return None, f"Graph error: {error}"
+
+            graph_obj = BioGraph(graph=pygraph)
+            fig = graph_obj.count_edge_by_data_source(plot=True)
+            return fig, None
+        except Exception as e:
+            return None, {"message": f"Error generating edge counts: {str(e)}"}
