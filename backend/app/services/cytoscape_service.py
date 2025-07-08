c621a5e76ca993e6f02d4882adaf82315ea77562
Tooba Abbassi-Daloii <t.abbassidaloii@maastrichtuniversity.nl>
2025-07-08 09:49:33 +0200

    add download capability

--- backend/app/services/cytoscape_service.py
+++ backend/app/services/cytoscape_service.py
@@ -1,16 +1,44 @@
 from pathlib import Path
-from pyBiodatafuse.graph import cytoscape
+from pyBiodatafuse.graph import cytoscape as cytoscape_graph
 from py4cytoscape import cytoscape_ping
 
+from fastapi import Depends, HTTPException
+from ..database import get_db
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from .graph_service import GraphService
 from .. import models
-
 
 class CytoscapeService:
     def __init__(self, db: AsyncSession):
         self.db = db
 
+    async def build_graph_for_cytoscape(
+            self,
+            set_id: int,
+            annotations: models.Annotation,
+            graph_dir: Path,
+    ) -> models.CytoscapeFile:
+        
+        cytoscape = models.CytoscapeFile(
+            identifier_set_id=set_id
+        )
+        self.db.add(cytoscape)
+        await self.db.commit()
+
+        pygraph, error = GraphService.create_pygraph(annotations, graph_dir)
+        if error:
+                return None, f"Graph error: {error}"
+
+        cytoscape_graph_json = cytoscape_graph.convert_graph_to_json(pygraph)
+        cytoscape.cytoscape_graph = cytoscape_graph_json
+        cytoscape.status = "completed"
+
+        await self.db.commit()
+        await self.db.refresh(cytoscape)
+
+        return cytoscape
+    
     async def load_graph_into_cytoscape(self, annotations: models.Annotation, graph_dir: Path, graph_name: str):
         try:
             if cytoscape_ping() != "You are connected to Cytoscape!":
@@ -23,7 +51,8 @@ class CytoscapeService:
             if error:
                 return {"success": False, "message": error}
 
-            cytoscape.load_graph(pygraph, network_name=graph_name)
+            cytoscape_graph.load_graph(pygraph, network_name=graph_name)
             return {"success": True, "message": f"Graph loaded into Cytoscape as '{graph_name}'."}
         except Exception as e:
             return {"success": False, "message": f"Error loading graph into Cytoscape: {str(e)}"}
+        
